c038ef5db7dc8610c3b2a28c9feed1ef9f75034e
Pierre Fontaine <pierrefontaine00@gmail.com>
2021-01-18 16:49:50 +0100

    :wrench: Add Cors on config file.

--- conduit/settings.py
+++ conduit/settings.py
@@ -25,6 +25,8 @@ class Config(object):
         'http://localhost:4200',
         'http://0.0.0.0:4000',
         'http://localhost:4000',
+        'http://0.0.0.0:' + os.environ.get('PORT', '8080'),
+        'https://0.0.0.0:' + os.environ.get('PORT', '8080'),
         'https://cranky-keller-ede639.netlify.app'
     ]
     JWT_HEADER_TYPE = 'Token'
